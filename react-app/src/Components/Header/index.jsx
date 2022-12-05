4f469437f77cf1c997e6d347e0b58f75e1e73079
Armen2552 <armenhayrapetyan@gmail.com>
2022-12-05 19:46:03 +0400

    05-22

---
+++ react-app/src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+const Header = ()=>{
+
+
+    return <div>
+        <p>Text</p>
+    </div>
+}
+
+export default Header
---
+++ react-app/src/Components/box/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+const Boxes = ()=>{
+
+    return <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, sapiente?</h1>
+    </div>
+}
+
+export default Boxes
